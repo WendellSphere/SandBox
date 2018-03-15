5764b0bfcd2e76819af9b3b5bac1e70b2f8dbed8
William Wendell <wwendell@sm.amark.com>
2018-03-15 11:26:58 -0700

    Add NewTheme

---
+++ SBox/ClientApp/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SBox.ClientApp
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~css").Include(
+                "~/ArcadiaTheme/css/animate.css"
+                , "~/ArcadiaTheme/css/boostrap.css"
+                , "~/ArcadiaTheme/css/jasny-bootstrap.min.css"
+                , "~/ArcadiaTheme/css/owl.carousel.css"
+                , "~/ArcadiaTheme/css/responsive.css"
+                , "~/ArcadiaTheme/css/style.css"
+                , "~/ArcadiaTheme/css/owl.theme.css"
+                , "~/ArcadiaTheme/css/owl.theme.css"
+                ));
+        }
+    }
+}
